bc323cde204f3e72aa4d479f6b7102b65bf1b32d
4FViP <152526348+4FViP@users.noreply.github.com>
2024-02-02 14:05:50 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from pyromod import listen
 from pyrogram import Client, filters, enums
 from kvsqlite.sync import Client as dt
 p = dict(root='plugins')
-tok = Config.TG_BOT_TOKEN ## توكنك 
-id = Config.APP_ID ## ايديك
+tok = 5901305358:AAFdYgavJ491TAWuxXQMpHqABIBWMjnPHCg
+id = 6110080048
 db = dt("data.sqlite", 'fuck')
 if not db.get("checker"):
   db.set('checker', None)
